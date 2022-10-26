471b9f6854a438c5052db029f2e42eb5a53d4b79
br <brunno00amorim@gmail.com>
2022-10-26 13:50:51 -0300

    :recycle: refactor: finalizando exemplo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 
 import PassandoProps from "./components/PassandoPros"
 import './App.css';
-// 1. Import the extendTheme function
-
-
 
 function App() {
   return (
--- src/components/PassandoPros.js
+++ src/components/PassandoPros.js
@@ -4,38 +4,48 @@ import { Button } from '@chakra-ui/react'
 import { useState } from 'react'
 import Task from './Task'
 import { Heading } from '@chakra-ui/react'
-
-
 //Componente recebendo valor do elemento pai (props)
 //observem: props.valor (mesmo nome do valor enviado pelo elemento pai)
-function PassandoProps(props) {
-
+function PassandoProps() {
     const [task, setTask] = useState("");
     const [arrayTask, setArrayTask] = useState([])
 
     const createTask = () => {
+    const idTask = Date.now();
+
         const newTask = {
-            tarefa: task
+            tarefa: task,
+            idTask
         } 
         const arrayToSpread = [...arrayTask, newTask]
-        setArrayTask(arrayToSpread)            
+        setArrayTask(arrayToSpread)
+        setTask("")
+        
     }
-
     const handleTask = (event) => {
         setTask(event.target.value)
-    }
+      }
 
-    const taskMapeado = arrayTask.map((task, index) => {
+      function deleteTask (id){
+         const filterToDelete = arrayTask.filter((task)=>{
+            if(id!== task.idTask){
+                return (task)
+            }      
+     })
+     setArrayTask(filterToDelete)
+     }
+     
+    const taskMapeado = arrayTask.map((task) => {
         return (
-            <Task key={index} tarefa={task.tarefa} ></Task>
+            <Task key={task.idTask} tarefa={task} deleteTask={deleteTask}></Task>
         )
-
     })
 
     return (<div id="divProps">
       <Heading m={5} >Lista de tarefas</Heading>
         <Box w='30%' m={5}   >
-            <Input placeholder='Qual sua próxima tarefa?' onChange={handleTask} />
+            <Input placeholder='Qual sua próxima tarefa?' onChange={handleTask} value={task} />
+            
                 <Button colorScheme='teal' size='sm' mt='2' w='100%'
                 onClick={createTask}>
                 Enviar
